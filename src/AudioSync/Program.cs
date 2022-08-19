d7094ccda5b62d9e92c4ac269a4bd1786307fa7c
Philippe Paré <phyyl@live.ca>
2022-08-18 22:45:31 -0400

    Initial source commit

---
+++ src/AudioSync/Program.cs
@@ -0,0 +1,85 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System.CommandLine;
+using System.Net;
+using System.Net.Sockets;
+
+Argument<string> hostArgument = new("host");
+Option<int> bufferOption = new("--buffer");
+Option<int> portOption = new("--port", () => 4411);
+
+Command hostCommand = new("host");
+hostCommand.Add(portOption);
+hostCommand.Add(bufferOption);
+hostCommand.SetHandler(Host, portOption, bufferOption);
+
+Command connectCommand = new("connect");
+connectCommand.Add(hostArgument);
+connectCommand.Add(portOption);
+connectCommand.Add(bufferOption);
+connectCommand.SetHandler(Connect, hostArgument, portOption, bufferOption);
+
+RootCommand rootCommand = new();
+rootCommand.Add(hostCommand);
+rootCommand.Add(connectCommand);
+rootCommand.Invoke(args);
+
+void Connect(string hostname, int port, int bufferMs)
+{
+    WasapiOut output = new(AudioClientShareMode.Shared, bufferMs);
+    BufferedWaveProvider provider = new(output.OutputWaveFormat);
+    TcpClient client = new(hostname, port);
+    Stream stream = client.GetStream();
+    byte[] buffer = new byte[1024 * 32];
+
+    client.NoDelay = true;
+
+    output.Init(provider);
+    output.Play();
+
+    while (!Console.KeyAvailable)
+    {
+        try
+        {
+            int read = stream.Read(buffer, 0, buffer.Length);
+            provider.AddSamples(buffer, 0, read);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            break;
+        }
+    }
+}
+
+void Host(int port, int bufferMs)
+{
+    WasapiLoopbackCapture capture = new(audioBufferMillisecondsLength: bufferMs);
+    List<TcpClient> clients = new();
+    TcpListener listener = new(IPAddress.Any, port);
+
+    capture.DataAvailable += (sender, e) =>
+    {
+        foreach (var client in clients.ToArray())
+        {
+            try
+            {
+                client.GetStream().Write(e.Buffer, 0, e.BytesRecorded);
+            }
+            catch
+            {
+                clients.Remove(client);
+            }
+        }
+    };
+
+    capture.StartRecording();
+    listener.Start();
+
+    while (!Console.KeyAvailable)
+    {
+        TcpClient client = listener.AcceptTcpClient();
+        client.NoDelay = true;
+        clients.Add(client);
+    }
+}
---
+++ src/AudioSync/WasapiLoopbackCapture.cs
@@ -0,0 +1,19 @@
+﻿using NAudio.CoreAudioApi;
+
+public class WasapiLoopbackCapture : WasapiCapture
+{
+    public WasapiLoopbackCapture(MMDevice? captureDevice = default, bool useEventSync = false, int audioBufferMillisecondsLength = 100)
+        : base(captureDevice ?? GetDefaultLoopbackCaptureDevice(), useEventSync, audioBufferMillisecondsLength)
+    {
+    }
+
+    public static MMDevice GetDefaultLoopbackCaptureDevice()
+    {
+        return new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+    }
+
+    protected override AudioClientStreamFlags GetAudioClientStreamFlags()
+    {
+        return AudioClientStreamFlags.Loopback | base.GetAudioClientStreamFlags();
+    }
+}
